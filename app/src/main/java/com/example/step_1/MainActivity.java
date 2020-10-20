eef1fef8979a3a24efffe57b10a8a6175bc35da0
iwan88 <biju88@yandex.ru>
2020-10-20 19:32:39 +0700

    Lesson_2

--- app/src/main/java/com/example/step_1/MainActivity.java
+++ app/src/main/java/com/example/step_1/MainActivity.java
@@ -1,11 +1,12 @@
 package com.example.step_1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -13,8 +14,21 @@ public class MainActivity extends AppCompatActivity {
 
         //My code section BEGIN
 
+        RecyclerView recyclerView = findViewById(R.id.recycler);
+
         System.out.println("Hello WORLD!!!"); //Lesson_1
+
+        RecyclerView recyclerView = findViewById(R.id.recycler);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter.setItems(items);
 
         //My code section END
     }
+    //My code BEGIN
+    public class ItemActivity extends AppCompatActivity{
+        ItemsAdapter adapter = new ItemsAdapter;
+    }
+
+    //My code END
 }
